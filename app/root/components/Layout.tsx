fdaad9c8d9ac3d7997ad56b8970949373c60bdb9
takeokunn <bararararatty@gmail.com>
2025-06-15 17:46:04 +0900

    refactor(Layout): remove unused stylesheet import

--- app/root/components/Layout.tsx
+++ app/root/components/Layout.tsx
@@ -1,7 +1,6 @@
 import { ColorSchemeScript, MantineProvider } from "@mantine/core";
 import "@mantine/core/styles.css";
 import { Scripts, ScrollRestoration } from "react-router";
-import appStylesHref from "../../app.css?url";
 
 export function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -10,7 +9,6 @@ export function Layout({ children }: { children: React.ReactNode }) {
         <meta charSet="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <ColorSchemeScript />
-        <link rel="stylesheet" href={appStylesHref} />
       </head>
       <body>
         <MantineProvider>
